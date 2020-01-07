a55f47ff96ccb2fee7be832d4314ff7a477903ff
Soren Martius <soeren.martius@gmail.com>
2020-01-06 22:55:46 -0300

    pre-commit hooks for terraform fmt, validate and tflint

---
+++ pre_commit_hooks/terraform/fmt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# set -e is considered to be a bad practice as written here http://mywiki.wooledge.org/BashFAQ/105
+# so we use a trap instead
+trap 'exit' ERR
+
+# Make environment variables working in OSX GUI apps such as Github Desktop https://stackoverflow.com/q/135688/483528
+export PATH=$PATH:/usr/local/bin
+
+terraform fmt -recursive
+
---
+++ pre_commit_hooks/terraform/validate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set -e is considered to be a bad practice as written here http://mywiki.wooledge.org/BashFAQ/105
+# so we use a trap instead
+trap 'exit' ERR
+
+# Make environment variables working in OSX GUI apps such as Github Desktop https://stackoverflow.com/q/135688/483528
+export PATH=$PATH:/usr/local/bin
+
+for dir in $(echo "$@" | xargs -n1 dirname | sort -u | uniq); do
+  terraform init -backend=false "$dir"
+  terraform validate "$dir"
+done
