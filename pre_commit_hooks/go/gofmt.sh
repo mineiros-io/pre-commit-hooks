6d916e5e60e29ce4852774bd7dc8d094e115b3ca
Soren Martius <soeren.martius@gmail.com>
2020-02-21 16:06:28 +0100

    add gofmt hook

---
+++ pre_commit_hooks/go/gofmt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Make environment variables working in OSX GUI apps such as Github Desktop
+# https://stackoverflow.com/q/135688/483528
+export PATH=$PATH:/usr/local/bin
+
+for file in "$@"; do
+  go fmt  "./$(dirname "$file")"
+done
