05bc62fdda87fe8fbfa94838cf2475076615b1f4
Soren Martius <soeren.martius@gmail.com>
2020-05-25 13:51:58 +0200

    remove old workaround for github desktop

--- pre_commit_hooks/make/phony-targets.sh
+++ pre_commit_hooks/make/phony-targets.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Make environment variables working in OSX GUI apps such as Github Desktop
-# https://stackoverflow.com/q/135688/483528
-export PATH=$PATH:/usr/local/bin
-
 EXITCODE=0
 
 validate_phony_targets() {
