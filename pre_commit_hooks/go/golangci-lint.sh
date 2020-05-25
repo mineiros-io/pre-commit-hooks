556e7c0194f0bfc49e19cde1553a84cc59e6d785
Soren Martius <soeren.martius@gmail.com>
2020-05-25 13:53:37 +0200

    remove old workaround for github desktop

--- pre_commit_hooks/go/golangci-lint.sh
+++ pre_commit_hooks/go/golangci-lint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# Make environment variables working in OSX GUI apps such as Github Desktop
-# https://stackoverflow.com/q/135688/483528
-export PATH=$PATH:/usr/local/bin
-
 if hash golangci-lint 2>/dev/null; then
     golangci-lint run --enable-all --fix "$@"
 else
--- pre_commit_hooks/terraform/fmt.sh
+++ pre_commit_hooks/terraform/fmt.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-# Make environment variables working in OSX GUI apps such as Github Desktop
-# https://stackoverflow.com/q/135688/483528
-export PATH=$PATH:/usr/local/bin
-
 terraform fmt -recursive
