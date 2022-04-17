f4cc6dac270e478680fc2167a256302dd69eee20
Marius Tolzmann <marius@mineiros.io>
2022-04-17 19:58:50 +0200

    chore: help to find markdown link checker binary

--- pre_commit_hooks/markdown/markdown-link-check.sh
+++ pre_commit_hooks/markdown/markdown-link-check.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if command -v npm >/dev/null 2>&1; then
+  prefix=$(npm config get prefix)
+  export PATH=${PATH}:${prefix}/bin
+fi
+
 if ! command -v markdown-link-check >/dev/null 2>&1; then
   echo >&2 "markdown-link-check is not available on this system."
   echo >&2 "Please install it by running 'npm install -g markdown-link-check'"
