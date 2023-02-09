6432f60f1108862c45bfafc6ce8b319b9695e458
Peter Dam <peter.dam@mineiros.io>
2023-02-09 20:05:26 +0100

    feat: terradoc on multiple files

--- pre_commit_hooks/terradoc/fmt.sh
+++ pre_commit_hooks/terradoc/fmt.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
 set -e
+set -u 
+set -o pipefail
 
-if ! command -v terradoc >/dev/null 2>&1; then
-  echo >&2 "terradoc is not available on this system."
-  echo >&2 "Please install it by running 'go install github.com/mineiros-io/terradoc/cmd/terradoc@latest'"
-  exit 1
-fi
+# shellcheck disable=SC2155,SC2034
+readonly ARGS=("$@")
 
-terradoc fmt $1 -w
+function is_binary_available() {
+  local bin_list=( terradoc )
+  for b in "${bin_list[@]}"; do
+    if [ -x "$(type -p "${b}")" ]; then
+      declare -g "_${b}"="$(type -p "${b}")"
+    else
+      echo >&2 "The executable ${b} is missing"
+      exit 1
+    fi
+  done
+}
+
+main() {
+  is_binary_available
+
+  for filename in "${ARGS[@]}"; do
+    # shellcheck disable=SC2154
+    "${_terradoc}" fmt -w "${filename}"
+  done
+}
+# Run script
+main
