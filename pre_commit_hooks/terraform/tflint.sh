3df7d5ec6b585a4491073223eb58dea2037e91d6
Esanim <17294241+Esanim@users.noreply.github.com>
2024-02-13 12:10:20 +0100

    fix: update tflint script to accept newer versions

--- pre_commit_hooks/terraform/tflint.sh
+++ pre_commit_hooks/terraform/tflint.sh
@@ -2,6 +2,22 @@
 
 set -e
 
-for file in "$@"; do
-  tflint "$file"
-done
+if ! command -v tflint >/dev/null 2>&1; then
+  echo >&2 "tflint is not available on this system."
+  echo >&2 "Please visit https://github.com/terraform-linters/tflint for instructions on how to install."
+  exit 1
+fi
+
+tflint_version=$(tflint -v | awk 'NR==1 { print $3 }' | awk -F. '{ print $1"."$2 }')
+max_old_version="0.46"
+
+if [[ $(tflint -v | awk -vmaxv=$max_old_version 'NR==1 { version=$3; if(version > maxv){print "true"} }') == "true" ]]; then
+  if [[ ! -n "$CI" ]]; then
+    tflint --init
+  fi
+  tflint
+else
+  for file in "$@"; do
+    tflint "$file"
+  done
+fi
